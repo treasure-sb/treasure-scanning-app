8c01d7f4570a39187269fb7545921671799d9a0b
nicfigu <nicfigu@gmail.com>
2024-06-22 00:19:09 -0400

    delete example img + folder

--- app-example/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { Slot } from 'expo-router';
-import React from 'react'
-
-const RootLayout = () => {
-  return <Slot />
-}
-
-export default RootLayout
