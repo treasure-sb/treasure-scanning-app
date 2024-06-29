6df9622801c2262babd52f20bf78a158561c766c
nicfigu <nicfigu@gmail.com>
2024-06-29 15:53:38 -0400

    i got the keys

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,8 +4,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 import { Database } from '@/types/supabase'
 
-const supabaseUrl = 'https://qkdlfshzugzeqlznyqfv.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrZGxmc2h6dWd6ZXFsem55cWZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAwMDA3MzYsImV4cCI6MjAxNTU3NjczNn0.TmjUj-7otNZgLWMWqYKV4x49mQlq41HqmwuJFgpfU6I'
+const supabaseUrl = process.env.REACT_APP_SUPA_URL
+const supabaseAnonKey = process.env.REACT_APP_SUPA_KEY
 export const userID = async() =>{
   return (await supabase.auth.getUser()).data.user?.id
 }
@@ -62,7 +62,7 @@ export type Vendor = {
   vendorsAtTable: Number
 };
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient<Database>(supabaseUrl as string, supabaseAnonKey as string, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
